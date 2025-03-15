e6aaaa29443f14b542c9c23376544064a63b3bd7
Richard <rlii0102@student.monash.edu>
2025-03-15 18:19:21 +1100

    alter authController
    - enables successful api call from frontend

--- project/backend/controllers/authController.js
+++ project/backend/controllers/authController.js
@@ -1,12 +1,10 @@
 import { Student } from "../schemas/studentSchema.js";
 
-export const login = async (req, res) => {
-  const { username, password } = req.body;
+export const Login = async (req, res) => {
+  const { email, password } = req.body;
 
   try {
-    const user = Student.find(
-      (u) => u.username === username && u.password === password
-    );
+    const user = await Student.findOne({ email: email, passwd: password });
 
     if (!user)
       return res.status(401).json({ success: false, data: "No user found" });
--- project/backend/routes/authRoute.js
+++ project/backend/routes/authRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
+import { Login } from "../controllers/authController.js";
 
 const authRouter = express.Router();
-authRouter.post("/login");
+authRouter.post("/login", Login);
 
 export default authRouter;
