55d51bb7e0c2368a93430998097dffe1bb164007
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-03-15 21:09:06 +1100

    SHENKIT adds back old method

--- project/backend/controllers/timetableController.js
+++ project/backend/controllers/timetableController.js
@@ -1,5 +1,4 @@
 import { Student } from "../schemas/studentSchema.js";
-import { getAllClassesForUnit } from "../algorithm/algorithmBackend.js";
 
 export const getTimetable = async (req, res) => {
   try {
@@ -14,14 +13,7 @@ export const getTimetable = async (req, res) => {
     const semesterNum = parseInt(semester, 10);
 
     // Find timetable for the given studentId
-    const student = await Student.findOne(
-      {
-        stuid: studentId,
-        "semesterEnrolment.year": yearNum,
-        "semesterEnrolment.semester": semesterNum,
-      },
-      { semesterEnrolment: 1 },
-    );
+    const student = await Student.findOne({ stuid: studentId });
 
     if (!student) {
       return res
@@ -29,11 +21,47 @@ export const getTimetable = async (req, res) => {
         .json({ success: false, data: "Student not found" });
     }
 
-    const semesterEnrolment = student.semesterEnrolment[0];
-    const units = semesterEnrolment.unitEnrolment.reduce(
-      (r, e) => r.push(e.unitcode),
-      [],
+    const units = student.semesterEnrolment.filter(
+      (sem) => sem.semester === semesterNum && sem.year === yearNum,
     );
+
+    return res.status(200).json({ success: true, data: units });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ success: false, error: "Server error", data: err.message });
+  }
+};
+
+export const getAllClassesForEnrolledUnits = async (req, res) => {
+  try {
+    const { studentId, year, semester } = req.params;
+    if (!studentId) {
+      return res
+        .status(400)
+        .json({ success: false, data: "Student ID is required in params" });
+    }
+
+    const yearNum = parseInt(year, 10);
+    const semesterNum = parseInt(semester, 10);
+
+    // Find timetable for the given studentId
+    const student = await Student.findOne({ stuid: studentId });
+
+    if (!student) {
+      return res
+        .status(404)
+        .json({ success: false, data: "Student not found" });
+    }
+
+    const semesterEnrolment = student.semesterEnrolment.filter(
+      (sem) => sem.semester === semesterNum && sem.year === yearNum,
+    );
+
+    const units = [];
+    for (const u of semesterEnrolment.unitEnrolment) {
+      units.push(u.unitcode);
+    }
 
     const unitsWithClasses = [];
     for (const unitcode in units) {
